71068c79a6c19ac5c58289233256cbb185696099
Den <den34092den@gmail.com>
2021-02-15 23:06:57 +0200

    commit

--- alert/alert.js
+++ alert/alert.js
@@ -1,20 +1,57 @@
-'use string'
+'use string';
 let money, time;
 money = +prompt("Ваш бюджет на месяц?");
 time = prompt("Введите дату в формате YYYY-MM-DD");
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        ans1: ans2
-    },
+    expenses: {},
     optionalExpenses: {
 
     },
     income: [],
     saving: false
 };
-ans1 = prompt("“Введите обязательную статью расходов в этом месяце");
-var ans2 = +prompt("“Во сколько обойдется?");
+
+for (let i = 0; i < 2; ++i) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+    if (typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50){
+        appData.expenses[a] = b;
+        console.log("[log] Success");
+    }
+    else{
+        i--;
+        console.log("[log] Write error");
+    }
+}
+//let i=0;
+// while(i<2){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//         b = prompt("Во сколько обойдется?");
+//     if (typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50){
+//         appData.expenses[a] = b;
+//         console.log("[log] Success");
+//     }
+//     else{
+//         i--;
+//         console.log("[log] Write error");
+//     }
+//     i++;
+// }
+// do{
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//         b = prompt("Во сколько обойдется?");
+//     if (typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50){
+//         appData.expenses[a] = b;
+//         console.log("[log] Success");
+//     }
+//     else{
+//         i--;
+//         console.log("[log] Write error");
+//     }
+//     i++;
+// }while(i<2);
 let days = 30;
-alert("Ваш бюджет: " + appData.budget / days);
+appData.moneyPerDay=appData.budget/days;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
