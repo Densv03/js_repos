d15487c3b63ec7465bd1ecfe70df52282ba37506
denissuhomlin <denis.suhomlin@b2b.discount>
2023-05-26 01:01:31 +0300

    add serializers

--- carRentNest/src/users/users.controller.ts
+++ carRentNest/src/users/users.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  public createUser(@Body() body: CreateUserDto): void {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  public async findUser(@Param('id') id: string): Promise<User> {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
--- carRentNest/src/users/users.service.ts
+++ carRentNest/src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  public create(email: string, password: string): Promise<User> {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  public findOne(id: number): Promise<User> {
+    return this.repo.findOne(id);
+  }
+  
+  public find(email: string): Promise<User[]> {
+    return this.repo.find({ email });
+  }
+
+  public async update(id: number, attrs: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  public async remove(id: number): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.repo.remove(user);
+  }
+}
